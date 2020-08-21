e213825683cf7ae419c3ddb5f0f49036b8d4d294
Vaheguru <shivangcool077@gmail.com>
2020-08-22 03:08:32 +0530

    second commit

--- public/js/zoomin.js
+++ public/js/zoomin.js
@@ -4,12 +4,13 @@ const zoomout=document.querySelector(".zoom-out");
 zoomin.addEventListener("click",function(e){
 
     ctx.scale(1.1,1.1);
-    ctx.translate(-50,-0);
+    ctx.translate(-10,-0);
     redraw();
 })
 zoomout.addEventListener("click",function(){
     
     ctx.scale(0.95,0.95);
-    ctx.translate(30,20)
+    ctx.translate(10,0)
+    //ctx.clearRect(0, 0, board.width, board.height);
     redraw();
 })
